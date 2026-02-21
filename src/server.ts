71af6c92bed1efc6aab7cf90288bed13946aca10
ASIF ALI <mdasifalijihat@gmail.com>
2026-02-21 13:13:20 +0600

    init

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import app from "./app";
+
+const port = 3000;
+
+// Start the server
+const bootstrap = () => {
+  try {
+      app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+      });
+  } catch (error) {
+    console.error("Failed to start server", error);
+  }
+};
+
+bootstrap()
